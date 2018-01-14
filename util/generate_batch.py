02a2ac1ea1a9d1444e57d5bab4329acfb1a8c1b0
ross <820584458@qq.com>
2018-01-14 14:47:30 +0800

    生成batch 函数

---
+++ util/generate_batch.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+# Author: Ross
+import pandas as pd
+import numpy as np
+
+
+def generate_batchs(data: pd.DataFrame, window_len=10):
+    '''
+    每window_len天的输入作为一个输入
+    :param data:
+    :param window_len:
+    :return:
+    '''
+    input_tmp = list()
+    output_tmp = list()
+    for i in range(len(data) - window_len - 1):
+        input_tmp.append(np.array(data[i:(i + window_len)].copy()))
+        output_tmp.append(np.array(data.iloc[i + window_len + 1].copy()))
+    inputs = np.array(input_tmp)
+    outputs = np.array(output_tmp)
+    return inputs, outputs
+
+
+def split_data(data: pd.DataFrame):
+    '''
+    将数据分成2/3训练集和测试集1/3
+    :param data:
+    :return:
+    '''
+    split_index = np.floor(len(data) * float(2 / 3)).astype(int)
+    train_set = data.iloc[:split_index]
+    test_set = data.iloc[split_index:]
+    generate_batchs(test_set)
+    return train_set, test_set
+
+
+if __name__ == '__main__':
+    split_data(pd.read_csv('../data/bitcoin-20130428-20180113.csv'))
