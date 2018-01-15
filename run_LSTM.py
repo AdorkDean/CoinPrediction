5f0357aa9acff7eae1ea0ec188a4d2ed3fab74e6
DevRoss <820584458@qq.com>
2018-01-15 23:30:49 +0800

    以每日差价作为输出训练 数据特征增加：每日差价 增加数据同一化，模型收敛加速

--- run_LSTM.py
+++ run_LSTM.py
@@ -14,8 +14,13 @@ RANDOM_SEED = 188
 def main():
     np.random.seed(RANDOM_SEED)  # 方便结果重现
     data = preprocessing_data.date2weekday(DATA_PATH)
+    data = preprocessing_data.add_increase_col(data=data)
+    # data = preprocessing_data.normalize_data(data=data,
+    #                                          colmns=['Weekday', 'High', 'Low', 'Volume', 'Market Cap', 'Close'])
+    data = preprocessing_data.normalize_data(data=data)
+    # print(data.head())
     training_set, test_set = generate_batch.split_data(data)
-    training_inputs, train_outputs = generate_batch.generate_batchs(training_set, colmns='Close')
+    training_inputs, train_outputs = generate_batch.generate_batchs(training_set, colmns='Increase')
     test_inputs, test_outputs = generate_batch.generate_batchs(test_set, colmns='Close')
     model = LSTM.build_model(inputs=training_inputs, output_size=1, LSTM_units=20)
     history = model.fit(training_inputs, train_outputs, batch_size=1, epochs=30, verbose=2, shuffle=True)
--- util/preprocessing_data.py
+++ util/preprocessing_data.py
@@ -2,6 +2,7 @@
 # Author: Ross
 import datetime
 import pandas as pd
+import numpy as np
 
 
 def date2weekday(path=None, data: pd.DataFrame = None):
@@ -19,5 +20,37 @@ def date2weekday(path=None, data: pd.DataFrame = None):
     data.rename(columns=lambda x: 'Weekday' if x == 'Date' else x, inplace=True)  # 修改索引名字
     return data
 
+
+def normalize_data(path=None, data: pd.DataFrame = None, colmns: list = None):
+    '''
+    标准化数据
+    :param path:
+    :param data:
+    :return:
+    '''
+    if path is None and data is None:
+        raise AssertionError('two parameters can not be blank at the same time.')
+    if path:
+        data = pd.read_csv(path)
+
+    if colmns:
+        for colmn in colmns:
+            data[colmns] = data[colmns].apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+    else:
+        data = data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+    return data
+
+
+def add_increase_col(path=None, data: pd.DataFrame = None):
+    if path is None and data is None:
+        raise AssertionError('two parameters can not be blank at the same time.')
+    if path:
+        data = pd.read_csv(path)
+
+    data['Increase'] = data['Close'] - data['Open']
+    return data
+
+
 if __name__ == '__main__':
-    date2weekday('../data/bitcoin-20130428-20180113.csv')
+    add_increase_col(data=date2weekday('../data/bitcoin-20130428-20180113.csv'))
+    # date2weekday('../data/bitcoin-20130428-20180113.csv')
